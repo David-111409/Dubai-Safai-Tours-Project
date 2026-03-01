8925283bee3ee1e63fa835559d87608ececc7196
David-111409 <muhammaddawoud610@gmail.com>
2026-03-01 04:56:26 +0200

    some improvements on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 
 function App() {
   return (
     <>
       <Header />
+      {/* <Routes>
+         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} /> 
+      </Routes> */}
     </>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,29 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
 import logo from '../../assets/iamges/logo.png';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const closeMenu = () => setIsMenuOpen(false);
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        navRef.current &&
+        !navRef.current.contains(event.target) &&
+        isMenuOpen
+      )
+        setIsMenuOpen(false);
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isMenuOpen]);
 
   return (
     <header className="header">
@@ -16,7 +36,7 @@ const Header = () => {
         </div>
       </div>
 
-      <nav>
+      <nav ref={navRef}>
         <div className="nav-menu">
           {isMenuOpen ? (
             <i className="bi bi-x-lg" onClick={() => setIsMenuOpen(false)}></i>
@@ -24,30 +44,30 @@ const Header = () => {
             <i className="bi bi-list" onClick={() => setIsMenuOpen(true)}></i>
           )}
         </div>
-        <ul className={`nav-list ${isMenuOpen ? ' open' : ' closed'}`}>
+        <ul className={`nav-list ${isMenuOpen ? 'open' : 'closed'}`}>
           <li className="item">
-            <Link className="nav-link" to="/">
+            <Link className="nav-link" to="/" onClick={closeMenu}>
               <i className="bi bi-house-fill"></i>
               Home
             </Link>
           </li>
 
           <li className="item">
-            <Link className="nav-link" to="/about">
+            <Link className="nav-link" to="/about" onClick={closeMenu}>
               <i className="bi bi-bank2"></i>
               About
             </Link>
           </li>
 
           <li className="item">
-            <Link className="nav-link" to="/books">
+            <Link className="nav-link" to="/login" onClick={closeMenu}>
               <i className="bi bi-arrow-right-square-fill"></i>
               Login
             </Link>
           </li>
 
           <li className="item">
-            <Link className="nav-link" to="/register">
+            <Link className="nav-link" to="/register" onClick={closeMenu}>
               <i className="bi bi-person-plus-fill"></i>
               Register
             </Link>
